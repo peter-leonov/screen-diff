d4d412b37f2f972071b32c8e2191b39a789d178f
Peter Leonov <gojpeg@gmail.com>
2016-03-12 22:28:54 +0100

    procedural

--- compare.rb
+++ compare.rb
@@ -2,50 +2,70 @@
 
 # raise 'three args needed: a b diff' unless ARGV.length == 3
 
-system('convert a.png -compress none a.pgm')
-system('convert b.png -compress none b.pgm')
-system('diff -U10000 a.pgm b.pgm > diff.pgm')
+def main
+  system('convert a.png -compress none a.pgm')
+  system('convert b.png -compress none b.pgm')
+  system('diff -U10000 --minimal a.pgm b.pgm > diff.pgm')
 
-diff = File.readlines('diff.pgm')
-raise 'empty diff' if diff.empty?
+  diff = File.readlines('diff.pgm')
+  raise 'empty diff' if diff.empty?
 
-head = diff.shift(3)
-raise 'wrong diff header' unless head[0]['---'] && head[1]['+++'] && head[2]['@@']
-raise 'wrong format' unless diff.shift['P2']
+  head = diff.shift(3)
+  raise 'wrong diff header' unless head[0]['---'] && head[1]['+++'] && head[2]['@@']
+  raise 'wrong format' unless diff.shift['P2']
 
-size_a = diff.shift
-raise 'TODO: do nothing for same size images' if size_a[0] == ' '
-size_b = diff.shift
-raise 'wrong sizes diff' unless size_a[0] == '-' && size_b[0] == '+'
-size_rex = /.(\d+) (\d+)/
-width_a = size_rex.match(size_a)[1].to_i
-width_b = size_rex.match(size_b)[1].to_i
-raise "different widths: #{width_a} and #{width_b}" if width_a != width_b
-width = width_a
+  size_a = diff.shift
+  raise 'TODO: do nothing for same size images' if size_a[0] == ' '
+  size_b = diff.shift
+  raise 'wrong sizes diff' unless size_a[0] == '-' && size_b[0] == '+'
+  size_rex = /.(\d+) (\d+)/
+  width_a = size_rex.match(size_a)[1].to_i
+  width_b = size_rex.match(size_b)[1].to_i
+  raise "different widths: #{width_a} and #{width_b}" if width_a != width_b
+  width = width_a
 
-spacer = '255 ' * width
+  colors = diff.shift
 
-a = []
-b = []
+  a, b = parse(width, diff)
 
-colors = diff.shift
+  write 'da.pgm', width, colors, a
+  write 'db.pgm', width, colors, b
 
-loop do
-  c = diff.shift or break
-  if c[0] == ' '
-    a << c[1..-1]
-    b << c[1..-1]
+  system('convert da.pgm da.png')
+  system('convert db.pgm db.png')
+end
+
+
+def parse width, diff
+  spacer = '255 ' * width + "\n"
+  a = []
+  b = []
+  
+  loop do
+    line = diff.shift or break
+    if line[0] == ' '
+      a << line[1..-1]
+      b << line[1..-1]
+    elsif line[0] == '-'
+      
+    elsif line[0] == '+'
+      a << spacer
+      b << line[1..-1]
+    end
+  end
+  
+  return a, b
+end
+
+def write name, width, colors, lines
+  File.open(name, 'w') do |f|
+    f.puts 'P2'
+    f.puts "#{width} #{lines.size}"
+    f.print colors
+    lines.each { |line| f.print line }
   end
 end
 
-da = File.open('da.pgm','w')
-# db = File.open('db.pgm','w')
-da.puts 'P2'
-da.puts "#{width} #{a.size}"
-da.print colors
-a.each { |line| da.print line }
-da.close
-
-system('convert da.pgm da.png')
+main
 
 # compare a.png b.png png:- | montage -geometry +4+0 a.png - b.png diff.png
