3f31bfe21822a0dc9ce449da93fbb07883e15394
Peter Leonov <gojpeg@gmail.com>
2016-03-13 18:48:45 +0100

    adapt for rgb.rb

--- compare.rb
+++ compare.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
 
 def main file_a, file_b, file_diff
-  system(*%W{convert #{file_a} -compress none tmp/a.pgm})
-  system(*%W{convert #{file_b} -compress none tmp/b.pgm})
+  system(*%W{./rgb.rb dump #{file_a} tmp/a.rgb})
+  system(*%W{./rgb.rb dump #{file_b} tmp/b.rgb})
 
-  diff = `diff -U10000 --minimal tmp/a.pgm tmp/b.pgm`.lines
+  diff = `diff -U10000 --minimal tmp/a.rgb tmp/b.rgb`.lines
   raise 'empty diff' if diff.empty?
 
   head = diff.shift(3)
   raise 'wrong diff header' unless head[0]['---'] && head[1]['+++'] && head[2]['@@']
-  raise 'wrong format' unless diff.shift['P2']
+  raise 'wrong format' unless diff.shift['RGB']
 
   size_a = diff.shift
   raise 'TODO: do nothing for same size images' if size_a[0] == ' '
@@ -25,10 +25,10 @@ def main file_a, file_b, file_diff
 
   a, b = parse(width, diff)
 
-  write 'tmp/da.pgm', width, colors, a
-  write 'tmp/db.pgm', width, colors, b
-  system('convert tmp/da.pgm tmp/da.png')
-  system('convert tmp/db.pgm tmp/db.png')
+  write 'tmp/da.rgb', width, colors, a
+  write 'tmp/db.rgb', width, colors, b
+  system('./rgb.rb load tmp/da.rgb tmp/da.png')
+  system('./rgb.rb load tmp/db.rgb tmp/db.png')
 
   system('compare tmp/da.png tmp/db.png tmp/diff.png')
   system(*%W{montage -geometry +4+0 #{file_a} tmp/diff.png #{file_b} #{file_diff}})
@@ -36,7 +36,7 @@ end
 
 
 def parse width, diff
-  spacer = '255 ' * width + "\n"
+  spacer = ([65535] * (width * 3)).join(' ') + "\n"
   a = []
   b = []
   minus = 0
@@ -82,7 +82,7 @@ end
 
 def write name, width, colors, lines
   File.open(name, 'w') do |f|
-    f.puts 'P2'
+    f.puts 'RGB'
     f.puts "#{width} #{lines.size}"
     f.print colors
     lines.each { |line| f.print line }
